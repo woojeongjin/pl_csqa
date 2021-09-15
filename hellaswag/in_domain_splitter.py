c9a248f235c05c5887e4da72aa731605c659fc05
Dongho Lee <danny911kr@naver.com>
2021-09-15 01:11:06 -0700

    data: hellaswag data update (reduce test)

--- hellaswag/in_domain_splitter.py
+++ hellaswag/in_domain_splitter.py
@@ -1,19 +1,24 @@
-# import json
-# import random
-#
-# with open('train.jsonl', 'r') as f:
-#     train_data = [json.loads(line) for line in f.readlines()]
-# with open('dev.jsonl', 'r') as f:
-#     dev_data = [json.loads(line) for line in f.readlines()]
-#
-# random.shuffle(train_data)
-# print(len(train_data))
-# print(len(dev_data))
+import json
+import random
 
-# with open('test.jsonl', 'w') as f:
-#     for item in train_data[:len(dev_data)]:
-#         f.write(json.dumps(item) + "\n")
-#
-# with open('train.jsonl', 'w') as f:
-#     for item in train_data[len(dev_data):]:
-#         f.write(json.dumps(item) + "\n")
+with open('hellaswag_train.jsonl', 'r') as f:
+    train_data = [json.loads(line) for line in f.readlines()]
+with open('hellaswag_val.jsonl', 'r') as f:
+    dev_data = [json.loads(line) for line in f.readlines()]
+
+random.shuffle(train_data)
+print(len(train_data))
+print(int(len(dev_data)*0.5))
+
+with open('test.jsonl', 'w') as f:
+    for item in train_data[:int(len(dev_data)*0.5)]:
+        f.write(json.dumps(item) + "\n")
+
+with open('train.jsonl', 'w') as f:
+    for item in train_data[int(len(dev_data)*0.5):]:
+        f.write(json.dumps(item) + "\n")
+
+with open('train.jsonl', 'r') as f:
+    new_train_data = [json.loads(line) for line in f.readlines()]
+
+print(len(new_train_data))
